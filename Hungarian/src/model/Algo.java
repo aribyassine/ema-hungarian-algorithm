9d778ef84f23b5aea2a1439f2c44eea5c920cf97
laurent.mouterde <laurent.mouterde@gmail.com>
2011-05-06 14:14:56 +0000

    interface algo

---
+++ Hungarian/src/model/Algo.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author laurent
+ */
+public class Algo implements AlgoInterface{
+
+	private int tab[][];
+	private boolean preference;
+	
+	public Algo(int[][] tab, boolean preference) {
+		init(tab, preference);
+	}
+
+	public void init(int[][] tab, boolean preference) {
+		this.tab = tab;
+		this.preference = preference;
+	}
+
+	public void step10Affect0() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step1SubstractRow() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step2SubstractCol() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step3MarkRow() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step4MarkCol() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step5MarkRowCol() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step6Iterate() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step7StrikeRowCol() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step8SubstractNoMark() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void step9AddMarkTwice() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public boolean isPreference() {
+		return preference;
+	}
+
+	public void setPreference(boolean preference) {
+		this.preference = preference;
+	}
+
+	public int[][] getTab() {
+		return tab;
+	}
+
+	public void setTab(int[][] tab) {
+		this.tab = tab;
+	}
+
+	private int chercheMin(int row)
+	{
+		int min=Integer.MAX_VALUE;
+
+		return min;
+	}
+
+}
---
+++ Hungarian/src/model/AlgoInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author laurent
+ */
+public interface AlgoInterface {
+	public void init(int tab[][], boolean preference);
+	public void step1SubstractRow();
+	public void step2SubstractCol();
+	public void step3MarkRow();
+	public void step4MarkCol();
+	public void step5MarkRowCol();
+	public void step6Iterate();
+	public void step7StrikeRowCol();
+	public void step8SubstractNoMark();
+	public void step9AddMarkTwice();
+	public void step10Affect0();
+}
