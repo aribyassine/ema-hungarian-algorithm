e2637d5efdfb46c5b1dc74e291dfa57a24e97210
laurent.mouterde@gmail.com <laurent.mouterde@gmail.com@cef60aa5-b2bb-bfbd-ed88-54ac7800bda1>
2011-05-09 07:13:17 +0000

    step 1 et 2 : soustraction ligne et colonne

--- trunk/Hungarian/src/model/Algo.java
+++ trunk/Hungarian/src/model/Algo.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools ?5| Templates
+ * and open the template in the editor.?3
  */
 
 package model;
@@ -11,7 +11,7 @@ package model;
  */
 public class Algo implements AlgoInterface{
 
-	private int tab[][];
+	private int tab[][]={{1,2,3,4,5},{1,4,2,5,3},{3,2,1,5,4},{1,2,3,5,4},{2,1,4,3,5}};
 	private boolean preference;
 	
 	public Algo(int[][] tab, boolean preference) {
@@ -24,28 +24,75 @@ public class Algo implements AlgoInterface{
 	}
 
 	public void step10Affect0() {
-		throw new UnsupportedOperationException("Not supported yet.");
+            throw new UnsupportedOperationException("Not supported yet.");
 	}
+
+        public void step10AffectZeroByRow(){
+            Integer nbZero=0, xZero=null, yZero=null;
+
+            for(int row=0;row<tab.length;row++)
+            {
+                for(int col=0;col<tab.length;col++)
+                {
+                    if(tab[row][col]==0)
+                    {
+                        xZero=row;
+                        yZero=col;
+                        nbZero++;
+                    }
+                }
+                if(nbZero==1){
+                    //COMMENT MARQUER LE ZERO
+                }
+            }
+        }
+
+        public void step10AffectZeroByCol(){
+            
+        }
 
 	public void step1SubstractAllRow() {
             for(int i=0;i<this.tab[0].length; i++)
             {
-                
+                step1SubstractRow(i);
             }
 	}
 
         public void step1SubstractRow(int row) {
-		throw new UnsupportedOperationException("Not supported yet.");
+            int valueToSobstract;
+            if(this.preference==true){
+                valueToSobstract=chercheMinRow(row);
+
+            }else{
+                valueToSobstract=chercheMaxRow(row);
+            }
+            for(int i=0;i<this.tab[row].length;i++)
+            {
+                tab[row][i]=tab[row][i]-valueToSobstract;
+            }
 	}
 
 	public void step2SubstractAllCol() {
-		throw new UnsupportedOperationException("Not supported yet.");
+            for(int i=0;i<this.tab[0].length; i++)
+            {
+                step2SubstractCol(i);
+            }
 	}
 
         public void step2SubstractCol(int col) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+            int valueToSobstract;
+            if(this.preference==true){
+                valueToSobstract=chercheMinCol(col);
 
+            }else{
+                valueToSobstract=chercheMaxCol(col);
+            }
+            for(int i=0;i<this.tab[col].length;i++)
+            {
+                tab[i][col]=tab[i][col]-valueToSobstract;
+            }
+	}
+        
 	public void step3MarkRow() {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
@@ -90,11 +137,15 @@ public class Algo implements AlgoInterface{
 		this.tab = tab;
 	}
 
-	private int chercheMin(int row)
+	private int chercheMinRow(int row)
 	{
-		int min=Integer.MAX_VALUE;
-                //coucou
-		return min;
+            int min=Integer.MAX_VALUE;
+            for(int i=0;i<tab[0].length;i++)
+            {
+                if(tab[row][i]<min)
+                min=tab[row][i];
+            }
+            return min;
 	}
 
     public void resolveMatrix()
@@ -110,6 +161,37 @@ public class Algo implements AlgoInterface{
         resolveMatrix();
         
         return null;
+    }
+
+    private int chercheMaxRow(int row)
+    {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<tab[0].length;i++)
+        {
+            if(tab[row][i]>max)
+                max=tab[row][i];
+        }
+        return max;
+    }
+
+    private int chercheMinCol(int col) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<tab[0].length;i++)
+        {
+            if(tab[i][col]<min)
+                min=tab[i][col];
+        }
+        return min;
+    }
+
+    private int chercheMaxCol(int col) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<tab[0].length;i++)
+        {
+            if(tab[i][col]>max)
+                max=tab[i][col];
+        }
+        return max;
     }
 
 }
