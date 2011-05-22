45d80f5ad13870aaf01c4c40e242bc67bcad0abf
andre.miras@gmail.com <andre.miras@gmail.com@cef60aa5-b2bb-bfbd-ed88-54ac7800bda1>
2011-05-22 12:56:23 +0000

    commited missing files

---
+++ trunk/Hungarian/src/model/AlgoModelListener.java
@@ -0,0 +1,16 @@
+/*
+ * Une interface ecouter pour permettant de notifier les vues des changements
+ */
+
+package model;
+
+/**
+ *
+ * @author andre
+ */
+public interface AlgoModelListener {
+
+    void algoModelChanged
+     (AlgoInterface algoModel);
+
+}
