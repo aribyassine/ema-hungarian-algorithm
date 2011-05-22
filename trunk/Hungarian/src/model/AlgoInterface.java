79596c8cdc8ca52395be11c9c73a8c18c67332b0
andre.miras@gmail.com <andre.miras@gmail.com@cef60aa5-b2bb-bfbd-ed88-54ac7800bda1>
2011-05-22 19:38:07 +0000

    cleaning interface up

--- trunk/Hungarian/src/model/AlgoInterface.java
+++ trunk/Hungarian/src/model/AlgoInterface.java
@@ -21,18 +21,7 @@ public interface AlgoInterface {
         
         public void addAlgoModelListener (final AlgoModelListener algoModelListener);
         public void removeAlgoModelListener(final AlgoModelListener algoModelListener);
-        
-	public void step1SubstractAllRow();
-	public void step2SubstractAllCol();
-        public boolean step3SelectMarkZero();
-	public void step4MarkRow();
-	public void step5MarkCol();
-	public void step6MarkRowCol();
-	public void step7Iterate();
-	public void step8StrikeRowCol();
-	public void step9SubstractNoMark();
-	public boolean step10Affect0Mark();
-	public boolean step11Affect0Soluce();
+
 
         /*
          * Call up the step in the right order
